5ed49626550d774757451c365355575e7f57054c
Nightingale111 <99151249+Nightingale111@users.noreply.github.com>
2024-10-11 20:45:21 -0400

    Added C++ file
    Examining a recursive addition program

---
+++ Quest_4-6.cpp
@@ -0,0 +1,11 @@
+// Quest_4-6.cpp - done
+/* This program takes in two integers from the user, which it then passes to
+* the function called "mystery()". This function takes in integers and its return 
+* type is an integer. This seems to be a recursive algorithm where the value of a
+* itself does not change through each iteration, but its value is added at each step.
+* The function calls itself with integers a = a and b = b - 1 until b = 1, at which
+* point it returns a plus all previous iterations. 
+* For example, if a = 2 and b = 4, the finished equation would be 2+(2+(2+(2))) and
+* the returned final result would be 8. 
+* Basically, the programmer is using recursion to add a to itself b times.
+*/
